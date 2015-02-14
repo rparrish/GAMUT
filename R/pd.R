05a92b46248e6c36ba5aea7c2a8c6ac55a308dd0
Rollie Parrish <rparrish@flightweb.com>
2015-02-14 14:44:47 -0800

    added dag & operator parameters.

--- R/pd.R
+++ R/pd.R
@@ -4,24 +4,43 @@
 #' Filters out incomplete cases and those with more denominator cases
 #' than numerators.
 #'
-#' @param ... comma separated list of variables to include. First variable should be
-#' group, which is either "program_name" or "ID".
+#' @param dag list of redcap data access groups. Shows the program name for the
+#' data access groups in the list and the anonymous ID for all others. If empty (default), then all
+#' program names will be shown.
+#' @param operator used to show multiple data access groups from the same operator (ie. AIM, MTR, etc.).
+#'  Will display the program names when the first three letters of the program name matches the value
+#'  in this field.
+#' @param ... comma separated list of variables to include.
 #' @author Rollie Parrish
 #' @export
 
-pd <- function(...) {
+pd <- function( dag = "", operator = "",  ...) {
     program_data <-
         mydata %>%
         filter(redcap_event_name != "Initial") %>%
-        select_(...) %>%
+        select_("program_name", "ID", "redcap_data_access_group", ...) %>%
         filter(!is.na(num),
                !is.na(den),
                den > 0,
                den >= num) %>%
-        group_by(group) %>%
-        summarise_each(funs(sum))
+        group_by(program_name, ID, redcap_data_access_group) %>%
 
-    program_data$mark <- 16
+        #
+        summarise_each(funs(sum)) %>%
+        ungroup() %>%
+        {
+            if(dag != "") {
+                mutate(., label = ifelse(redcap_data_access_group %in% dag, as.character(program_name), ID))
+            } else {mutate(., label = program_name)}
+        } %>%
+        {
+            if(operator != "") {
+                mutate(., label = ifelse(substr(as.character(program_name), 1,3) == operator, as.character(program_name),label ))
+            } else {.}
+        }
+
+
+    program_data$mark = 16
 
     results <- program_data
 
