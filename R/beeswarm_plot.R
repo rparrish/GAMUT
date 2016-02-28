6db9818720e816fe3de35139153d6455e8a14d76
rparrish <rparrish@flightweb.com>
2016-02-28 12:59:29 -0800

    added logic for no data for the individual program

--- R/beeswarm_plot.R
+++ R/beeswarm_plot.R
@@ -25,30 +25,30 @@ beeswarm_plot <- function(data = NULL, title = "GAMUT Metric title")  {
                            sum(data$den), ")"),method="hex"
     )
 
-    indiv_points <-
-        data %>%
-        filter(program_name == params$program_name) %>%
-        with(., prop.test(num, den))
+        # plot points for individual program
+        indiv_points <-
+            data %>%
+            filter(program_name == params$program_name)
 
-    arrows(x0 = indiv_points$conf.int[1], y0=1.3,
-           x1 = indiv_points$conf.int[2], y1=1.3,
-           code = 3, angle = 90, col = "blue",
-           length =.1)
-    points(y = 1.3,x=indiv_points$estimate, pch=19, cex = 1.2, col = "blue") # add mean
+        if(nrow(indiv_points) > 0) {
+            indiv_points %>%
+                with(., prop.test(num, den))
 
-    overall_mean <- sum(data$num)/sum(data$den)
+            arrows(x0 = indiv_points$conf.int[1], y0=1.3,
+                   x1 = indiv_points$conf.int[2], y1=1.3,
+                   code = 3, angle = 90, col = "blue",
+                   length =.1)
+            points(y = 1.3,x=indiv_points$estimate, pch=19, cex = 1.2, col = "blue") # add mean
 
+            # add text labels
+            text(x = indiv_points$estimate, y = 1.45, paste0("  ", round(indiv_points$estimate,3)*100,"%"),
+                 col = "blue")
+        }
     # add mean
     segments(y0 = .6, x0=overall_mean,
              y1 = 1.4, x1 = overall_mean,
              pch=16, lty = "dashed",
              col = "darkgrey")
-
-    # add text labels
-    text(x = indiv_points$estimate, y = 1.45, paste0("  ", round(indiv_points$estimate,3)*100,"%"),
-         col = "blue")
-
-    #text(x = overall_mean, y = .55, paste0("Overall: ", round(overall_mean,3)*100,"%"))
 
     axis(1, at=pretty(data$num/data$den),
          lab=paste0(pretty(data$num/data$den) * 100, "%"),
